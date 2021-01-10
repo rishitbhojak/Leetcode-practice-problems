7038987cac25ff6c11aa6067ff037f332dd072aa
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-10 23:42:05 +0530

    brute force approach

--- two-sum.c
+++ two-sum.c
@@ -1,7 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+int *twoSum(int *nums, int numsSize, int target, int *returnSize){
+    *returnSize = 2;
+    int *returnValues = malloc((*returnSize) * sizeof(int));
 
-int main()
-{
-    return 0;
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                returnValues[0] = i;
+                returnValues[1] = j;
+                break;
+            }
+        }
+    }
+    
+    return returnValues;
 }
