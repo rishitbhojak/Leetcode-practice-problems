748af395e2372566e302ba330f7ba7181cee2065
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-11 18:42:27 +0530

    optimized solution

--- palindrome_number.c
+++ palindrome_number.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    return 0;
+bool isPalindrome(int x) {
+bool arr[2] = { true, false };
+int revertedNumber = 0;
+int tmp = x;
+
+if (x < 0 || (x % 10 == 0 && x != 0)) 
+	return false;
+
+while (x > revertedNumber) {
+	revertedNumber = revertedNumber * 10 + x % 10;
+	x /= 10;
+}
+
+if (x == revertedNumber || x == revertedNumber / 10)
+	return true;
+else
+	return false;
 }
